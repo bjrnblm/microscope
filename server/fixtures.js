cb85d1155a3d0472a53f80358f891d38664f91a0
Björn Blom <bjornblom@gmail.com>
2014-10-30 00:26:05 +0100

    detail route and user registration

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,19 @@
 if (Posts.find().count() === 0) {
     Posts.insert({
         title: 'Bjorns awesome site',
-        url: 'http://bjornblom.com'
+        url: 'http://bjornblom.com',
+        author: 'bjorn'
     });
 
     Posts.insert({
         title: 'NoProtocol',
-        url: 'http://noprotocol.nl'
+        url: 'http://noprotocol.nl',
+        author: 'bjorn'
     });
 
     Posts.insert({
         title: 'Bike Ok',
-        url: 'http://www.bikeok.nl'
+        url: 'http://www.bikeok.nl',
+        author: 'kees'
     });
 }
